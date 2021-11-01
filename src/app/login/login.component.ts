b4d725aa0581f6686b8536d18857e8eb1130b769
Everson Ortega <everson_helltram@hotmail.com>
2021-11-01 08:24:13 -0300

    alt

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
+import { StringMap } from '@angular/compiler/src/compiler_facade_interface';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {AuthService } from '../auth.service';
+import { Usuario } from './usuario';
 
 @Component({
   selector: 'app-login',
@@ -12,9 +15,11 @@ export class LoginComponent  {
   password: string;
   loginError: boolean;
   cadastrando: boolean;
+  mensagemSucesso: string;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private authService : AuthService
   ) { }
 
   onSubmit(){
@@ -27,6 +32,28 @@ export class LoginComponent  {
   }
   cancelaCadastro(){
     this.cadastrando = false;
+  }
+
+  cadastrar(){
+    const usuario: Usuario = new Usuario();
+    usuario.username = this.username;
+    usuario.password = this.password;
+    this.authService
+        .salvar(usuario)
+        .subscribe( response => {
+          this.mensagemSucesso= "Cadastro realizado com sucesso! Efetue o login";
+          this.loginError=false;
+          this.cadastrando=false;
+          this.username ='';
+          this.password = '';
+          setTimeout(()=>{
+            this.mensagemSucesso= '';
+          },2000)
+        },error =>{
+          this.loginError= true;
+          this.mensagemSucesso=null;
+        }
+        )
   }
 
 }
